bd85deab432707ad6185319731a1dfd9a45df2d0
Colton Gyulay <coltongyulay@gmail.com>
2015-12-01 15:43:51 -0500

    changed default test to small

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import markov
 
 print 'Building model...this could take a few seconds.\n'
 
-# corpus_path = 'crunchbase_descriptions_small.txt'
-corpus_path = 'crunchbase_descriptions.txt'
+corpus_path = 'crunchbase_descriptions_small.txt'
+# corpus_path = 'crunchbase_descriptions.txt'
 token_size = 2
 
 model = markov.Generator(corpus_path, token_size)

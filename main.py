6bed66964565a034930d944692d438f77349683d
Colton Gyulay <coltongyulay@gmail.com>
2015-12-10 10:40:16 -0500

    updated main to use 50k tagged corpus

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print 'Building model...this could take a few seconds.\n'
 # corpus_path = 'corpora/crunchbase_descriptions.txt'
 # corpus_path = 'corpora/crunchbase_descriptions_small.txt'
 # corpus_path = 'corpora/punctuation_test.txt'
-corpus_path = 'tagged_corpora/crunchbase_descriptions_25000.txt'
+corpus_path = 'tagged_corpora/crunchbase_descriptions_50000.txt'
 
 token_size = 3
 multi_sent = False
